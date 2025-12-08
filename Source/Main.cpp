b03c038f0dda347641db92a5b228df01fecf0157
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-12-08 23:11:09 +0100

    Initial setup with 75 FPS, signature in the bottom right corner and full screen

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -8,6 +8,77 @@
 // Projekat je dozvoljeno pisati počevši od ovog kostura
 // Toplo se preporučuje razdvajanje koda po fajlovima (i eventualno potfolderima) !!!
 // Srećan rad!
+
+unsigned signatureTex;
+
+unsigned int VBOsignature;
+
+void preprocessTexture(unsigned& texture, const char* filepath) {
+    texture = loadImageToTexture(filepath); // Učitavanje teksture
+    glBindTexture(GL_TEXTURE_2D, texture); // Vezujemo se za teksturu kako bismo je podesili
+
+    // Generisanje mipmapa - predefinisani različiti formati za lakše skaliranje po potrebi (npr. da postoji 32 x 32 verzija slike, ali i 16 x 16, 256 x 256...)
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // Podešavanje strategija za wrap-ovanje - šta da radi kada se dimenzije teksture i poligona ne poklapaju
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // S - tekseli po x-osi
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // T - tekseli po y-osi
+
+    // Podešavanje algoritma za smanjivanje i povećavanje rezolucije: nearest - bira najbliži piksel, linear - usrednjava okolne piksele
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void formVAO(float* vertices, size_t size, unsigned int& vao) {
+    // formiranje VAO-ova je izdvojeno u posebnu funkciju radi čitljivijeg koda u main funkciji
+
+    // Podsetnik za atribute:
+    /*
+        Jedan VAO se vezuje za jedan deo celokupnog skupa verteksa na sceni.
+        Na primer, dobra praksa je da se jedan VAO vezuje za jedan VBO koji se vezuje za jedan objekat, odnosno niz temena koja opisuju objekat.
+
+        VAO je pomoćna struktura koja opisuje kako se podaci u nizu objekta interpretiraju.
+        U render-petlji, za crtanje određenog objekta, naredbom glBindVertexArray(nekiVAO) se određuje koji se objekat crta.
+
+        Potrebno je definisati koje atribute svako teme u nizu sadrži, npr. pozicija na lokaciji 0 i boja na lokaciji 1.
+
+        Ova konfiguracija je specifična za naš primer na vežbama i može se menjati za različite potrebe u projektu.
+
+
+        Atribut se opisuje metodom glVertexAttribPointer(). Argumenti su redom:
+            index - identifikacioni kod atributa, u verteks šejderu je povezan preko svojstva location (location = 0 u šejderu -> indeks tog atributa treba staviti isto 0 u ovoj naredbi)
+            size - broj vrednosti unutar atributa (npr. za poziciju su x i y, odnosno 2 vrednosti; za boju r, g i b, odnosno 3 vrednosti)
+            type - tip vrednosti
+            normalized - da li je potrebno mapirati na odgovarajući opseg (mi poziciju već inicijalizujemo na opseg (-1, 1), a boju (0, 1), tako da nam nije potrebno)
+            stride - koliko elemenata u nizu treba preskočiti da bi se došlo od datog atributa u jednom verteksu do istog tog atributa u sledećem verteksu
+            pointer - koliko elemenata u nizu treba preskočiti od početka niza da bi se došlo do prvog pojavljivanja datog atributa
+    */
+    // Četvorougao
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &VBOsignature);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOsignature);
+    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+
+    // Atribut 0 (pozicija):
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Atribut 1 (boja):
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+}
+
+void drawSignature(unsigned int shader, unsigned int vao) {
+    glUseProgram(shader);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, signatureTex);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+}
+
 int main()
 {
     glfwInit();
@@ -16,7 +87,10 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Kostur", NULL, NULL);
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Bus Route Tracker", monitor, NULL);
     if (window == NULL) return endProgram("Prozor nije uspeo da se kreira.");
     glfwMakeContextCurrent(window);
 
@@ -25,14 +99,39 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glClearColor(0.2f, 0.8f, 0.6f, 1.0f);
+    preprocessTexture(signatureTex, "../Resources/signature.png");
+
+    unsigned int signatureShader = createShader("../Shaders/signature.vert", "../Shaders/signature.frag");
+    glUseProgram(signatureShader);
+    glUniform1i(glGetUniformLocation(signatureShader, "signatureTex"), 0);
+
+    float verticesSignature[] = {
+        0.5f, -0.7f, 0.0f, 1.0f, // gornje levo teme
+        0.5f, -1.0f, 0.0f, 0.0f, // donje levo teme
+        1.0f, -1.0f, 1.0f, 0.0f, // donje desno teme
+        1.0f, -0.7f, 1.0f, 1.0f, // gornje desno teme
+   };
+
+    unsigned int VAOsignature;
+    formVAO(verticesSignature, sizeof(verticesSignature), VAOsignature);
+
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
     while (!glfwWindowShouldClose(window))
     {
+        double initFrameTime = glfwGetTime();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
         glClear(GL_COLOR_BUFFER_BIT);
+
+        drawSignature(signatureShader, VAOsignature);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        while (glfwGetTime() - initFrameTime < 1 / 75.0) {}
     }
 
     glfwDestroyWindow(window);
